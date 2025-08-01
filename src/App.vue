3aaf46dc3407e0e1130ae17b8e935ee0875fd647
dingchenkai <993486200@qq.com>
2025-08-01 14:46:24 +0800

    Trigger deployment

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
 import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
+
 </script>
 
 <template>
